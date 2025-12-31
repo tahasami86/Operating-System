575ce1b864d90a591e3d1412f6fedb866413240d
Taha Sami <tahasami8@gmail.com>
2025-12-31 17:44:45 +0500

    Made changes to the Makefile && kernel.c
    Signed-off-by: Taha Sami <tahasami8@gmail.com>

--- kernel-simple/kernel.c
+++ kernel-simple/kernel.c
@@ -4,5 +4,10 @@ void dummy_test_entrypoint() {
 
 void main() {
     char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    // *video_memory = 'X'; privoulst it was overwiting the 'L' in the previous message
+    video_memory[70] = 'X'; 
+    video_memory[71] = 0x07; // light grey on black background
+
+    video_memory[72] = '0';
+    video_memory[73] = 0x07;
 }
