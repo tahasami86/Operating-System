ba763cae2e4a7d57293ee94b1769002506e4cead
Taha Sami <tahasami8@gmail.com>
2026-01-06 16:33:07 +0500

    Added Reversing of the number
    Signed-off-by: Taha Sami <tahasami8@gmail.com>

--- video-driver/kernel/util.c
+++ video-driver/kernel/util.c
@@ -8,15 +8,24 @@ void memory_copy(char *source, char *dest, int nbytes){
 }
 
 void int_to_ascii(int n, char str[]) {
-    int i, sign;
-    if ((sign = n) < 0) n = -n;
-    i = 0;
-    do {
-        str[i++] = n % 10 + '0';
-    } while ((n /= 10) > 0);
+    int i,sign;
+    if((sign = n) < 0) n= -n;
+    
+    i=0;
+    while(n > 0){
+        str[i++] = (n %10) + '0';
+        n /=10 ;
+    }
 
-    if (sign < 0) str[i++] = '-';
+    if(sign < 0) str[i++] = '-';
     str[i] = '\0';
 
-    /* TODO: implement "reverse" */
+    char tmp;
+    int j=0;
+    while(i > j){
+        tmp = str[--i];
+        str[i] = str[j];
+        str[j] = tmp;
+        j++;
+    }
 }
