9fae2d21420af39bc2ecaa0ab6819909d311a1e1
Taha Sami <tahasami8@gmail.com>
2026-01-14 14:44:36 +0500

    Added kmalloc
    Signed-off-by: Taha Sami <tahasami8@gmail.com>

--- shell/libc/string.c
+++ shell/libc/string.c
@@ -24,6 +24,28 @@ void int_to_ascii(int n, char str[]) {
     }
 }
 
+
+void hex_to_ascii(int n, char str[]) {
+    append(str, '0');
+    append(str, 'x');
+    char zeros = 0;
+
+    int32_t tmp;
+    int i;
+    for (i = 28; i > 0; i -= 4) {
+        tmp = (n >> i) & 0xF;
+        if (tmp == 0 && zeros == 0) continue;
+        zeros = 1;
+        if (tmp > 0xA) append(str, tmp - 0xA + 'a');
+        else append(str, tmp + '0');
+    }
+
+    tmp = n & 0xF;
+    if (tmp >= 0xA) append(str, tmp - 0xA + 'a');
+    else append(str, tmp + '0');
+}
+
+
 int strlen(char s[]){
     int i=0;
     while (s[i] != '\0')
